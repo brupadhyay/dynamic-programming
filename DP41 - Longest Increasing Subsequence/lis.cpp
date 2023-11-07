04002efeeea99d67acde622b7eaea7931efff0af
Bhavesh Upadhyay <bh8692080@gmail.com>
2023-11-07 15:45:18 +0530

    DP42: Printing LIS

---
+++ DP41 - Longest Increasing Subsequence/lis.cpp
@@ -0,0 +1,129 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * Memoization - Top-Down
+ * TC - O(N x N)
+ * SC - O(N) + O(N x N)
+ * O(N) for recursive stack space
+*/
+
+int lis(int i, int prev, vector<int>& nums, vector<vector<int>> &dp){
+    if(i == nums.size()) return 0;
+
+    if(dp[i][prev+1] != -1) return dp[i][prev+1];
+
+    int len = 0 + lis(i+1, prev, nums, dp);
+    if(prev == -1 || nums[i] > nums[prev]){
+        len = max(len, 1 + lis(i+1, i, nums, dp));
+    }
+    return dp[i][prev+1] = len;
+}
+
+int lengthOfLIS1(vector<int>& nums) {
+    int n = nums.size();
+    vector<vector<int>> dp(n, vector<int> (n+1, -1));
+    return lis(0, -1, nums, dp);
+}
+
+/**
+ * Tabulation - Bottom-Up
+ * TC - O(N x N)
+ * SC - O(N x N)
+*/
+
+int lengthOfLIS2(vector<int>& nums) {
+    int n = nums.size();
+    vector<vector<int>> dp(n+1, vector<int> (n+1, 0));
+    for(int i = n-1; i >= 0; i--){
+        for(int j = i-1; j >= -1; j--){
+            int len = 0 + dp[i+1][j+1];
+            if(j == -1 || nums[i] > nums[j]){
+                len = max(len, 1 + dp[i+1][i+1]);
+            }
+            dp[i][j+1] = len;
+        }
+    }
+    return dp[0][0];
+}
+
+/**
+ * Space Optimisation - 2D to 1D array Bottom-Up
+ * TC - O(N x N)
+ * SC - O(N) x 2
+*/
+
+int lengthOfLIS3(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> curr(n+1, 0);
+    vector<int> next(n+1, 0);
+    for(int i = n-1; i >= 0; i--){
+        for(int j = i-1; j >= -1; j--){
+            int len = 0 + next[j+1];
+            if(j == -1 || nums[i] > nums[j]){
+                len = max(len, 1 + next[i+1]);
+            }
+            curr[j+1] = len;
+        }
+        next = curr;
+    }
+    return curr[0];
+}
+
+/**
+ * Single Array Space Solution 
+ * Not much intuitive
+ * TC - O(N x N)
+ * SC - O(N)
+*/
+
+int lengthOfLIS4(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> dp(n, 1);
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < i; j++){
+            if(nums[i] > nums[j]) dp[i] = max(dp[i], 1 + dp[j]);
+        }
+    }
+    return *max_element(begin(dp), end(dp));
+}
+
+
+vector<int> printingLongestIncreasingSubsequence(vector<int> arr, int n) {
+	vector<int> dp(n, 1);
+	int lastIndex = 0;
+	int maxi = 0;
+	vector<int> hash(n, 0);
+	for(int i = 0; i < n; i++) hash[i] = i;
+	for(int i = 0; i < n; i++){
+		for(int prev = 0; prev < i; prev++){
+			if(arr[i] > arr[prev]){
+				if(1 + dp[prev] > dp[i]){
+					dp[i] = 1 + dp[prev];
+					hash[i] = prev;
+				}
+			}
+		}
+		if(dp[i] > maxi){
+			maxi = dp[i];
+			lastIndex = i;
+		}
+	}
+	vector<int> lis;
+	while(lastIndex != hash[lastIndex]){
+		lis.push_back(arr[lastIndex]);
+		lastIndex = hash[lastIndex];
+	}
+	lis.push_back(arr[lastIndex]);
+	reverse(lis.begin(), lis.end());
+	return lis;
+}
+
+int main(){
+    vector<int> arr = {10, 9, 2, 5, 3, 7, 101, 18};
+    int n = arr.size();
+    vector<int> ans = printingLongestIncreasingSubsequence(arr, n);
+    for(auto it: ans){
+        cout << it << " ";
+    }
+}
